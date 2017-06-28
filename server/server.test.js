0e8386c6707163294a826baab76c4a95223e59c4
Daniel Endres <danielendres@gmail.com>
2017-06-28 15:56:09 -0700

    use describe to group and structure tests

--- server/server.test.js
+++ server/server.test.js
@@ -3,30 +3,35 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return 404 with Page not found message', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
-});
+describe('Server', () => {
 
-// make a new test
-// assert 200
-// assert that you exits in users array
-it('user dan should exist in array', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Dan',
-        age: 35
-      });
-    })
-    .end(done);
+  describe('GET /', () => {
+    it('should return 404 with Page not found message', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: 'Page not found.'
+          });
+        })
+        .end(done);
+    });
+  });
+
+  describe('GET /users', () => {
+    it('user dan should exist in array', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            name: 'Dan',
+            age: 35
+          });
+        })
+        .end(done);
+    });
+  });
+
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,38 +2,46 @@ const expect = require('expect');
 
 const utils = require('./utils')
 
-it('should add two numbers', () => {
-  var res = utils.add(33, 11);
+describe('Utils', () => {
+  describe('Add', () => {
+    it('should add two numbers', () => {
+      var res = utils.add(33, 11);
 
-  expect(res).toBe(44).toBeA('number');
-  // if (res != 44) {
-  //   throw new Error(`Expected 44, but got ${res}`);
-  // }
-});
+      expect(res).toBe(44).toBeA('number');
+      // if (res != 44) {
+      //   throw new Error(`Expected 44, but got ${res}`);
+      // }
+    });
 
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
+    it('should async add two numbers', (done) => {
+      utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+      });
+    });
   });
+
+  describe('Square', () => {
+    it('should square a number', () => {
+      var res = utils.square(5);
+
+      expect(res).toBe(25).toBeA('number');
+      // if (res != 25) {
+      //   throw new Error(`Expected 25, but got ${res}`)
+      //}
+    });
+
+    it('should async square a number', (done) => {
+      utils.asyncSquare(5, (squared) => {
+        expect(squared).toBe(25).toBeA('number');
+        done();
+      })
+    });
+  });
+
 });
 
 
-it('should square a number', () => {
-  var res = utils.square(5);
-
-  expect(res).toBe(25).toBeA('number');
-  // if (res != 25) {
-  //   throw new Error(`Expected 25, but got ${res}`)
-  //}
-});
-
-it('should async square a number', (done) => {
-  utils.asyncSquare(5, (squared) => {
-    expect(squared).toBe(25).toBeA('number');
-    done();
-  })
-});
 
 // test to check that first and last names are set
 it('first and last name should be set to a string', () => {
