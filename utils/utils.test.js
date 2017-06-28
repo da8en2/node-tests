fa8a8332abd9cd0261ed73a2e8503e5aa45d1196
Daniel Endres <danielendres@gmail.com>
2017-06-28 14:57:11 -0700

    initial commit

---
+++ utils/utils.test.js
@@ -0,0 +1,50 @@
+const expect = require('expect');
+
+const utils = require('./utils')
+
+it('should add two numbers', () => {
+  var res = utils.add(33, 11);
+
+  expect(res).toBe(44).toBeA('number');
+  // if (res != 44) {
+  //   throw new Error(`Expected 44, but got ${res}`);
+  // }
+});
+
+it('should square a number', () => {
+  var res = utils.square(5);
+
+  expect(res).toBe(25).toBeA('number');
+  // if (res != 25) {
+  //   throw new Error(`Expected 25, but got ${res}`)
+  //}
+});
+
+// test to check that first and last names are set
+it('first and last name should be set to a string', () => {
+  var user = {location: "Phoenix", age: 35};
+  var res = utils.setName(user, 'Daniel Endres');
+
+  expect(res).toInclude({
+    firstName: 'Daniel',
+    lastName: 'Endres'
+  });
+});
+
+// it('should pass the test, just testing different test scenarios', () => {
+//   expect(12).toNotBe(13);
+//   // for objects and arrays need to use toEqual or toNotEqual!!!
+//   expect({name: 'dan'}).toEqual({name: 'dan'});
+//   expect({name: 'dan'}).toNotEqual({name: 'Dan'});
+//   //check if array or object includes something
+//   expect([1,2,3]).toInclude(2);
+//   expect([1,2,3]).toExclude(6);
+//
+//   expect({
+//     name: 'Dan',
+//     age: 35,
+//     location: 'Phoenix'
+//   }).toInclude({
+//     age: 35
+//   });
+// });
